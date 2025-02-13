39bde83a8814cafa7486f3db3bd25cd08edcb282
TrentQuarantino <teotozzi@gmail.com>
2025-02-13 09:28:50 +0100

    completely update add majoe changes

--- linkstore.py
+++ linkstore.py
@@ -1,63 +1,72 @@
-import streamlit as st
+import requests
 import datetime
+import streamlit as st
 import duck_utils as du
-import re
+
+def get_data():
+  response = requests.get('http://localhost:8888/to_app').json()  
+  return response['title'], response['url']
 
 du.create_table()
+
 uro = []
-cat_opt = ["Database", "Streamlit", "Python", "Fluter", "English", "Music"]
+cat_opt = ["Database", "Streamlit", "Python", "Fluter", "Angular", "English", "Music"]
 st.set_page_config(layout="wide")
+# headert
 colu1, colu2 = st.columns(2)
 with colu1:
   st.title(":mag: Link Store")
+  st.button('get data')
 with colu2:
   st.header("Store your link in DuckDb database")
+
 col1, col2 = st.columns([3,3], border=False)
+# insertion form
 with col1:
-  with st.expander("About this app"):
-    st.write('This app store, retry and show your link')
-  with st.form("entry_form", clear_on_submit=True):
+  with st.form("entry_form", enter_to_submit=False, clear_on_submit=True):
     oggi = datetime.datetime.now()
-    st.write("Today's Links &nbsp;&nbsp; ", oggi.strftime("%c"))
     tod_date = oggi.strftime("%c")
-
+    st.write("Today's Links &nbsp;&nbsp; ", tod_date)
+    
     cat = st.selectbox("Choose a Category", cat_opt,  key='category')
-    tit = st.text_input("Paste the article's Title")
-    url = st.text_input('Paste link URL', 'https://youtu.be/JwSS70SZdyM', key='url')
-    uro = url.split("=")
+    # get the data from browser 
+    title, url = get_data()
 
-    submitted = st.form_submit_button("Save Data")
-    if submitted:
+    url = st.text_input("",url)
+    mod_title = st.text_input('Edit if the title is not correct or imprecise', title)
+    if mod_title != title:
+      title =  mod_title
+
+    subm = st.form_submit_button('Save Data')
+    if subm:
+      du.save_links(tod_date, cat, title, url)
+      st.success("You succesfully saves this data: ")
       st.write(f"Data: {tod_date}")
       st.write(f"Category: {cat}")
-      st.write(f"Title: {tit}")
+      st.write(f"Titolo: {title}")
       st.write(f"URL: {url}")
-      #x = re.sub("watch\?v\=", "embed/", url)
-      #print(x)
-      #st.success("Data Saved!")
-      du.save_links(tod_date, cat, tit, url)
-      st.success(f"Data Saved")
 
-  
-    #li = du.show_all()
-    #st.dataframe(li,use_container_width=True, hide_index=True, column_config={1:'data', 2:'category', 3:'title', 4:'link'})
-    #st.table(li)
   with col2:
-   # VIDEO_URL = "https://www.youtube.com/watch?v=k3DBmAlUh1A"
-    #st.video(VIDEO_URL)
+    if url.find("youtube") > 0:
+      print(True)
+      uro = url.split("=")
+      ur = uro[-1]
+    else:
+      print(False)
+      uro = "oMHLkcc9I9c"
     
-    col2.markdown(
-    """
-    <div style="width: 600px; height: 400px; background-color: #ccc; border: 1px solid black; 
-               transform: perspective(500px) rotatey(-15deg);">
-               <iframe width="600" height="400"
-                  src="https://www.youtube.com/embed/{ur}">
-                </iframe>
-    </div>
-    """.format(ur = uro[-1]),
-    unsafe_allow_html=True,
-)
-
+    st.markdown(
+      """
+      <div style="width: 600px; height: 400px; background-color: #ccc; border: 1px solid black; 
+                  transform: perspective(500px) rotatey(-15deg);">
+                  <iframe width="600" height="400" 
+                    src="https://www.youtube.com/embed/{ur}">
+                  </iframe>
+      </div>
+      """.format(ur = uro[-1]),
+      unsafe_allow_html=True,
+    )
+   
 st.header("Show Links stored")
 sub = st.button("Show")
 if sub:
