f30c9a3162d41e27821e32bc2477cb133b19032d
TrentQuarantino <teotozzi@gmail.com>
2025-02-13 09:06:09 +0100

    modify db name insertion

--- duck_utils.py
+++ duck_utils.py
@@ -1,7 +1,9 @@
 import duckdb
 import streamlit as st
-def create_table():
-  with duckdb.connect('linko.db') as conn:
+
+DBNAME = 'links.db'
+def create_table(DBNAME):
+  with duckdb.connect(DBNAME) as conn:
     conn.sql('''CREATE TABLE IF NOT EXISTS links(
       data VARCHAR,
       category VARCHAR,
@@ -11,12 +13,12 @@ def create_table():
     )
 
 def save_links(data, category, title, url):
-  with duckdb.connect('linko.db') as conn:
+  with duckdb.connect(DBNAME) as conn:
     query1 = "INSERT INTO links VALUES (?,?,?,?)"
     conn.execute(query1, [data, category, title, url])
 
 def show_all():
-  with duckdb.connect('linko.db') as conn:
+  with duckdb.connect(DBNAME) as conn:
     #li = pd.read_sql_query("SELECT * FROM links",conn)
     li = conn.sql("SELECT * FROM links")#.fetchall()
     #li.fetchall()
