340a6171ec9d8c40074de93cc58bcdeb7e18b728
TrentQuarantino <teotozzi@gmail.com>
2025-02-14 10:49:28 +0100

    added search function

--- duck_utils.py
+++ duck_utils.py
@@ -26,5 +26,14 @@ def show_all():
     lidf = st.dataframe(li, use_container_width=True, hide_index=True, column_config={"link":st.column_config.LinkColumn()})
     return lidf
   
-# TODO search by title or  author
-   
+def search_by_keywords(keywords):
+  with duckdb.connect(DBNAME) as conn:
+    query = "SELECT * FROM links WHERE title LIKE '%{s}%'".format(s=keywords)
+    sear = conn.sql(query)
+    search_res = st.dataframe(sear, use_container_width=True,hide_index=False, column_config={"link":st.column_config.LinkColumn()})
+    return search_res
+  
+def update_title(new_name, link):
+  with duckdb.connect(DBNAME) as conn:
+    queryUp = "UPDATE links SET title = '{n}' WHERE link = '{l}'".format(n=new_name, l=link)
+    conn.execute(queryUp)
